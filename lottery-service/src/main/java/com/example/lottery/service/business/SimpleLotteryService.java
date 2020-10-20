7beab1526ac26236a5190d57a578a1e6b04457fa
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-20 09:54:45 +0300

    Add two implementations of RandomNumberService: SimpleRandomNumberService and FastRandomNumbersService Add qualifacation annotation: ServiceQuality

--- lottery-service/src/main/java/com/example/lottery/service/business/SimpleLotteryService.java
+++ lottery-service/src/main/java/com/example/lottery/service/business/SimpleLotteryService.java
@@ -8,14 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lottery.service.LotteryService;
+import com.example.lottery.service.QualityLevel;
 import com.example.lottery.service.RandomNumberService;
+import com.example.lottery.service.ServiceQuality;
 
 @Service
 public class SimpleLotteryService implements LotteryService {
-	@Autowired private RandomNumberService randomNumberService;
+	@Autowired 
+	@ServiceQuality(QualityLevel.SECURE)
+	private RandomNumberService randomNumberService;
 	
 	@Override
 	public List<List<Integer>> draw(int column) {
+		System.err.println(randomNumberService.getClass().getName());
 		return IntStream.range(0, column)
 				   .mapToObj(i -> draw())
 				   .collect(Collectors.toList());
