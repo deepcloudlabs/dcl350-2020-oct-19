76c99a62a21268aea8f139d1c9f9e7975120bb25
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-21 14:36:46 +0300

    implement findById and findByPage in SimpleCrmService

--- crm-microservice/src/main/java/com/example/crm/service/business/SimpleCrmService.java
+++ crm-microservice/src/main/java/com/example/crm/service/business/SimpleCrmService.java
@@ -1,9 +1,13 @@
 package com.example.crm.service.business;
 
+import static java.util.stream.Collectors.toList;
+
 import java.util.List;
 import java.util.Map;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.example.crm.dto.AddCustomerRequest;
@@ -11,23 +15,29 @@ import com.example.crm.dto.AddCustomerResponse;
 import com.example.crm.dto.CustomerResponse;
 import com.example.crm.dto.UpdateCustomerRequest;
 import com.example.crm.dto.UpdateCustomerResponse;
+import com.example.crm.entity.Customer;
 import com.example.crm.repository.CustomerRepository;
 import com.example.crm.service.CrmService;
 
 @Service
 public class SimpleCrmService implements CrmService {
     @Autowired private CustomerRepository customerRepository;
+    @Autowired private ModelMapper modelMapper;
     
 	@Override
 	public CustomerResponse findById(String identity) {
 		return customerRepository.findById(identity)
-				     .map( customer -> );
+				     .map(this::convertCustomerToCustomerReponse )
+				     .orElseThrow(() -> new IllegalArgumentException("Cannot find customer"));
 	}
 
 	@Override
 	public List<CustomerResponse> findByPage(int page, int size) {
-		// TODO Auto-generated method stub
-		return null;
+		return customerRepository.findAll(PageRequest.of(page, size))
+				.getContent()
+				.stream()
+				.map(this::convertCustomerToCustomerReponse)
+				.collect(toList());
 	}
 
 	@Override
@@ -53,5 +63,8 @@ public class SimpleCrmService implements CrmService {
 		// TODO Auto-generated method stub
 		return null;
 	}
-   
+	
+    private CustomerResponse convertCustomerToCustomerReponse(Customer customer) {
+    	return modelMapper.map(customer,CustomerResponse.class);
+    }
 }
