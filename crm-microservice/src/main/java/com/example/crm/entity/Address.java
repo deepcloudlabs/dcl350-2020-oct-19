e4a50e79ca7138343ce3aa91eaf54579e45ad0da
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-21 11:58:01 +0300

    add default constructor and setter/getter methods to entity classes

--- crm-microservice/src/main/java/com/example/crm/entity/Address.java
+++ crm-microservice/src/main/java/com/example/crm/entity/Address.java
@@ -1,11 +1,15 @@
 package com.example.crm.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "addresses")
 public class Address {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,4 +20,62 @@ public class Address {
 	private String zipCode;
 	@Enumerated(EnumType.STRING)
 	private AddressType type;
+
+	public Address() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getLine() {
+		return line;
+	}
+
+	public void setLine(String line) {
+		this.line = line;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public AddressType getType() {
+		return type;
+	}
+
+	public void setType(AddressType type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [id=" + id + ", city=" + city + ", country=" + country + ", line=" + line + ", zipCode="
+				+ zipCode + ", type=" + type + "]";
+	}
+
 }
--- crm-microservice/src/main/java/com/example/crm/entity/Customer.java
+++ crm-microservice/src/main/java/com/example/crm/entity/Customer.java
@@ -6,10 +6,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="customers")
+@Table(name = "customers")
 public class Customer {
 	@Id
 	private String identity;
@@ -17,7 +18,66 @@ public class Customer {
 	private String sms;
 	private String email;
 	@Lob
-	@Column(columnDefinition="longblob")
+	@Column(columnDefinition = "longblob")
 	private byte[] photo;
+	@OneToMany
 	private List<Address> addresses;
+
+	public Customer() {
+	}
+
+	public String getIdentity() {
+		return identity;
+	}
+
+	public void setIdentity(String identity) {
+		this.identity = identity;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public String getSms() {
+		return sms;
+	}
+
+	public void setSms(String sms) {
+		this.sms = sms;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+	public List<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(List<Address> addresses) {
+		this.addresses = addresses;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [identity=" + identity + ", fullname=" + fullname + ", sms=" + sms + ", email=" + email
+				+ ", addresses=" + addresses + "]";
+	}
+
 }
