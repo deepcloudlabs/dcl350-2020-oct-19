903e2119367ad0a4997d9534e49ae33b675fcc37
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-22 14:30:47 +0300

    initial commit

---
+++ lottery-service-javase-client/src/com/example/binance/client/MarketApiAsyncHttpClient.java
@@ -0,0 +1,38 @@
+package com.example.binance.client;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MarketApiAsyncHttpClient {
+
+	private static final String URL = "https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT";
+
+	public static void main(String[] args) throws IOException, InterruptedException {
+		var counter = new AtomicInteger(0);
+		var client = HttpClient.newHttpClient();
+		var request = HttpRequest.newBuilder()
+				.uri(URI.create(URL))
+				.header("Accept", "application/json")
+				.build();
+		var start = System.currentTimeMillis();
+		for (var i=0;i<10;++i) {
+			// asynchronous -> non-blocking
+			client.sendAsync(request, HttpResponse.BodyHandlers.ofString()) 
+					             .thenAccept( ticker -> {
+					            	 if(counter.incrementAndGet()==10) {
+					            		 var stop = System.currentTimeMillis();
+					            		 System.err.println("Duration: "+(stop-start)+" ms.");
+					            	 }
+					            	 System.err.println(Thread.currentThread().getName()+": "+ticker.body());								            	 
+					             });
+		}
+		System.err.println("exited from for loop...");
+		TimeUnit.SECONDS.sleep(5);
+	}
+
+}
---
+++ lottery-service-javase-client/src/com/example/binance/client/MarketApiWebsocketClient.java
@@ -0,0 +1,45 @@
+package com.example.binance.client;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.WebSocket;
+import java.net.http.WebSocket.Listener;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.TimeUnit;
+
+public class MarketApiWebsocketClient {
+
+	private static final String URL = "wss://stream.binance.com:9443/ws/btcusdt@trade";
+
+	public static void main(String[] args) throws InterruptedException {
+		Listener listener = new MarketWebSocketLister();
+		HttpClient.newHttpClient()
+		          .newWebSocketBuilder()
+                  .buildAsync(URI.create(URL ), listener );
+		TimeUnit.MINUTES.sleep(1);
+	}
+
+}
+
+class MarketWebSocketLister implements Listener {
+
+	@Override
+	public void onOpen(WebSocket webSocket) {
+		System.err.println("Connected to the market websocket api.");
+		webSocket.request(1);
+	}
+
+	@Override
+	public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+		System.err.println(data);
+		webSocket.request(1);
+		return null;
+	}
+
+	@Override
+	public CompletionStage<?> onClose(WebSocket webSocket, int statusCode, String reason) {
+		System.err.println("Disconnected from the market api.");
+		return null;
+	}
+	
+}
