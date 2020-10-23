ee65ccdeac2f465144c50c92a2049458e64d7e5f
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-23 15:54:33 +0300

    add config

---
+++ security-card-service/src/main/java/com/example/card/SecurityCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityCardServiceApplication.class, args);
+	}
+
+}
