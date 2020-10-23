10b74b717e05ed82fdc53d1f1ee33dadc55992fd
deepcloudlabs <deepcloudlabs@gmail.com>
2020-10-23 08:27:31 +0300

    remove error controller from swagger ui

--- hr-microservice/src/main/java/com/example/hr/config/SwaggerConfig.java
+++ hr-microservice/src/main/java/com/example/hr/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package com.example.hr.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -41,7 +42,7 @@ public class SwaggerConfig implements WebMvcConfigurer {
     public Docket api(ServletContext servletContext) {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
                 .host(host.concat(":").concat(Long.toString(port)))
